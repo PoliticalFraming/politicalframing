0cb2eb5ff73c4a8b939188c462133a038fb6fb6b
Al Johri <al.johri@gmail.com>
2014-01-06 02:29:45 -0600

    more aws script changes

--- aws.sh
+++ aws.sh
@@ -2,25 +2,32 @@
 
 export PFURL=ec2-54-224-242-94.compute-1.amazonaws.com
 export PFPEM=~/Desktop/politicalframing.pem
-export PFREPO=~/Dropbox/Development/politicalframing
 
 alias pf="ssh -i $PFPEM ubuntu@$PFURL"
+# Create 1GB swap space
 pf "sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024"
 pf "sudo /sbin/mkswap /var/swap.1"
 pf "sudo /sbin/swapon /var/swap.1"
+# Set environment variables
 pf "echo 'export PFURL=$PFURL' >> ~/.bashrc"
 pf "echo 'export DOKKU_REPO=https://github.com/AlJohri/dokku.git' >> ~/.bashrc"
 pf "echo 'export STACK_URL=https://github.com/AlJohri/buildstep.git' >> ~/.bashrc"
+# Install dokku
 pf "wget -qO- https://raw.github.com/progrium/dokku/v0.2.1/bootstrap.sh | sudo BUILD_STACK=true bash"
+# Set VHOST
 pf "export PFURL=$PFURL; sudo sh -c  'echo $PFURL > /home/dokku/VHOST'"
+# Install dokku plugins
 pf "sudo git clone https://github.com/statianzo/dokku-shoreman.git /var/lib/dokku/plugins/dokku-shoreman"
 pf "sudo git clone https://github.com/AlJohri/dokku-pg-plugin.git /var/lib/dokku/plugins/postgresql"
 pf "sudo git clone https://github.com/AlJohri/dokku-redis-plugin /var/lib/dokku/plugins/redis"
 pf "sudo dokku plugins-install"
+# Create databases
 pf "sudo dokku postgresql:create politicalframing"
 pf "sudo dokku redis:create politicalframing"
+# Set SSH key
 cat ~/.ssh/id_rsa.pub | pf "sudo sshcommand acl-add dokku progrium"
-cd $PFREPO && git remote add aws "dokku@$PFURL:politicalframing"
+# Add git remote
+git remote add aws "dokku@$PFURL:politicalframing"
 
 # git push aws master
 # pf "sudo dokku config:set politicalframing HEROKU=1"
