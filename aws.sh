a708937eb5fd56cd6ed124266c51a0ea8cbff897
Al Johri <al.johri@gmail.com>
2014-01-06 09:22:29 -0600

    changed aws.sh and added .env file

--- aws.sh
+++ aws.sh
@@ -3,54 +3,32 @@
 export PFURL=ec2-54-224-242-94.compute-1.amazonaws.com
 export PFPEM=~/Desktop/politicalframing.pem
 alias pf="ssh -i $PFPEM ubuntu@$PFURL"
-# Create 1GB swap space
+
 pf "sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024"
 pf "sudo /sbin/mkswap /var/swap.1"
 pf "sudo /sbin/swapon /var/swap.1"
-# Set environment variables
 pf "echo 'export PFURL=$PFURL' >> ~/.bashrc"
-# Install dokku
-pf "wget -qO- https://raw.github.com/progrium/dokku/v0.2.1/bootstrap.sh | sudo DOKKU_REPO=https://github.com/AlJohri/dokku.git STACK_URL=https://github.com/AlJohri/buildstep.git BUILD_STACK=true bash"
-# Set VHOST
+pf "wget -qO- https://raw.github.com/progrium/dokku/v0.2.1/bootstrap.sh | sudo bash"
 pf "export PFURL=$PFURL; sudo sh -c  'echo $PFURL > /home/dokku/VHOST'"
-# Install dokku plugins
 pf "sudo git clone https://github.com/statianzo/dokku-shoreman.git /var/lib/dokku/plugins/dokku-shoreman"
 pf "sudo git clone https://github.com/teemow/dokku-pg-plugin /var/lib/dokku/plugins/postgresql"
 pf "sudo git clone https://github.com/luxifer/dokku-redis-plugin /var/lib/dokku/plugins/redis"
 pf "sudo dokku plugins-install"
-# Create databases
-pf "sudo dokku postgresql:create politicalframing"
-pf "sudo dokku redis:create politicalframing"
-# Set SSH key
+pf "sudo dokku postgresql:create $PFURL"
+pf "sudo dokku redis:create $PFURL"
 cat ~/.ssh/id_rsa.pub | pf "sudo sshcommand acl-add dokku progrium"
-# Add git remote
-git remote add aws "dokku@$PFURL:politicalframing"
+git remote add aws "dokku@$PFURL:$PFURL"
+git push aws master
+dokku run python createdb.py
 
-# git push aws master
-# pf "sudo dokku config:set politicalframing HEROKU=1"
-# pf "sudo dokku config:set politicalframing C_FORCE_ROOT='true'"
-
-# REMEMBER TO SEED DATABASE NOW!!!!!
-
-
-# dokku run python createdb.py
-
-# OR
-
-# sudo docker run -i -t app/politicalframing:latest /bin/bash
-# export HEROKU=1
-# export DATABASE_URL= ((get value from dokku config politicalframing))
-# export REDIS_URL= ((get value from dokku config politicalframing))
-# /app/.heroku/python/bin/python /app/createdb.py
+# I changed the script to use the default buildstep and use the scipy/numpy buildpack using an environment variable.
+# I also changed the declaration of environment variables to be within a .env file.
+# pf "wget -qO- https://raw.github.com/progrium/dokku/v0.2.1/bootstrap.sh | sudo STACK_URL=https://github.com/AlJohri/buildstep.git BUILD_STACK=true bash"
 
 # Information
 # EC2 Instance AMI: ami-ef795786
 # EC2 Instance AMI url: https://console.aws.amazon.com/ec2/home?region=us-east-1#launchAmi=ami-ef795786
 # EC2 Instance Finder: http://cloud-images.ubuntu.com/locator/ec2/
 # Search Parameters: Ubuntu 13.04 raring amd64 ebs us-east-1
-
-
-
-
 
 # https://github.com/progrium/dokku/wiki/Recipes#specifying-a-custom-buildpack
