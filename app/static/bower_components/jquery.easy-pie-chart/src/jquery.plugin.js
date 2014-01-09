51cdff19e9ce5c8cac30418f6b7171ef1935f324
Al Johri <al.johri@gmail.com>
2014-01-09 06:08:45 -0600

    Removed bower_components from repository

--- app/static/bower_components/jquery.easy-pie-chart/src/jquery.plugin.js
+++
@@ -1,7 +0,0 @@
-$.fn.easyPieChart = function(options) {
-	return this.each(function() {
-		if (!$.data(this, 'easyPieChart')) {
-			$.data(this, 'easyPieChart', new EasyPieChart(this, options));
-		}
-	});
-};
