d7387ba20f747d0cf8dde4b364b5c199536fd7fa
Al Johri <al.johri@gmail.com>
2014-01-12 05:40:34 -0600

    Added createdb command to manage.py

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 1. vagrant box add \ precise64 http://files.vagrantup.com/precise64.box
 2. vagrant up
 
+web: gunicorn -w 1 -t 600 app:app
+
+for gunicorn to show errors use DEBUG = True
 
 
 https://github.com/gipi/dokku-django-example
--- manage.py
+++ manage.py
@@ -1,6 +1,13 @@
 from flask.ext.script import Manager
 from flask.ext.assets import ManageAssets
-from app import app, assets
+from app import app, assets, db
+
+from peewee import *
+from app.models.Frame import Frame, populate_frames_dummy_data
+from app.models.Speech import Speech
+from app.models.Topic import Topic
+from app.models.SpeechTopic import SpeechTopic
+from app.models.User import User
 
 manager = Manager(app)
 
@@ -10,5 +17,41 @@ manager.add_command("assets", ManageAssets(assets))
 def hello():
     print "hello"
 
+@manager.command
+def createdb():
+	Frame.create_table(fail_silently=True)
+	Speech.create_table(fail_silently=True)
+	Topic.create_table(fail_silently=True)
+	SpeechTopic.create_table(fail_silently=True)
+	User.create_table(fail_silently=True)
+	db.database.execute_sql("ALTER TABLE speech_topic ADD CONSTRAINT speech_topic_unique UNIQUE(speech_id,topic_id);")
+	populate_frames_dummy_data()
+
+
 if __name__ == "__main__":
     manager.run()
+
+
+### SEEEEEEED DATA
+# import os 
+
+# #Delete the database
+# try:
+# 	os.remove('database.db')
+# 	print "Database Deleted"
+# except:
+# 	pass
+
+# #Create new DB
+# execfile('createdb.py')
+# print "new database created"
+
+# #Populate Frames
+# from app.models.Frame import populate_frames_dummy_data
+# populate_frames_dummy_data()
+
+# #Populate Speeches
+# from app.database_views import download_speeches_for_topic
+# download_speeches_for_topic('gay')
+# download_speeches_for_topic('tomato')
+# download_speeches_for_topic('potato')
