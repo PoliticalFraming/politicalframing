1a579ccee916683c1ab22e23711891ea44a4c4a4
Al Johri <al.johri@gmail.com>
2014-02-22 02:19:32 -0600

    Basic modal for viewing a speech body.

--- app/static/scripts/directives/speechBrowser.js
+++ app/static/scripts/directives/speechBrowser.js
@@ -4,8 +4,33 @@ angular.module('framingApp').directive('speechBrowser', function() {
   return {
     restrict: 'E',
     transclude: true,
-    controller: function ($scope) {
+    controller: function ($scope, $modal, $log) {
       $scope.headers = ['ID', 'Title', 'Date', 'Speaker', 'State'];
+
+      $scope.currentSpeech = null;
+      
+      $scope.open = function (index) {
+
+        $scope.currentSpeech = $scope.speeches[index];
+
+        var modalInstance = $modal.open({
+          templateUrl: 'partials/speech-modal.html',
+          controller: 'ModalInstanceCtrl',
+          resolve: {
+            currentSpeech: function () {
+              return $scope.currentSpeech;
+            }
+          }
+        });
+
+        // modalInstance.result.then(function (selectedItem) {
+        //   $scope.selected = selectedItem;
+        // }, function () {
+        //   $log.info('Modal dismissed at: ' + new Date());
+        // });
+
+      };
+
     },
     scope: {
       speeches: '=',
