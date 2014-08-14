103b38b16699d13d9b10bb5486a676756273fb11
Mehta, Dhrumil <dhrumil.mehta@gmail.com>
2014-08-14 02:38:36 -0400

    add file to test packages in /lib

--- lib/packagetester.py
+++ lib/packagetester.py
@@ -1,9 +1,14 @@
 """
 This file contains code that tests the packages in lib folder.
 """
-from framemaker import framemaker
-from framemaker import newframemaker
-from framemaker import frameextender
-from framemaker import preprocessor
-from framemaker import wordnettools
 
+# How frames are currently made:
+# ==============================
+# * User enters a seed word
+#		ex. 'crime'
+# * Program finds all wordnet 'synsets' related to that word and ask the user if each is relevant
+#		ex. 'crime.n.01'
+# * For each relevant synset, program gets a list of synsets containing derivationally related forms to synset
+# 		ex. [Synset('accuse.v.01'), Synset('transgress.v.01'), Synset('criminal.s.03'), Synset('outlaw.v.01'), Synset('incriminate.v.01')]
+
+from framemaker import *
