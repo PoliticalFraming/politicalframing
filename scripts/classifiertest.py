ae36eefe800ebf43b80c0aa17faa01209fd0ba47
Mehta, Dhrumil <dhrumil.mehta@gmail.com>
2014-08-20 02:04:20 -0400

    changed a log

--- scripts/classifiertest.py
+++ scripts/classifiertest.py
@@ -10,7 +10,7 @@ from app.models.analysis import Analysis
 from sklearn.cross_validation import cross_val_score
 from numpy import array
 
-topic = "gun"
+topic = "immigration"
 
 # Get speeches for a particular topic
 print("Get speeches for topic '%s'" % topic)
@@ -18,12 +18,13 @@ speech_dicts = Speech.get(5000, 0, phrase=topic)['speeches']
 speeches = map(lambda x: Speech(**x), speech_dicts)
 speeches = filter(lambda x: x.speaker_party == 'D' or x.speaker_party == 'R', speeches)
 print("Found %d speeches with a party." % len(speeches))
+
 # Build Classifier
 print("Create Classifier")
 naive_bayes = Classifier()
 
 # Build Targets
-print("Building Bunch")
+print("Build target vector and data vector from documents")
 def party_fn(speech):
     if speech.speaker_party == 'D':
         return 1
@@ -31,7 +32,6 @@ def party_fn(speech):
         return 0
     else:
         raise Exception("Speech must have party 'D' or 'R': " + str(speech.speech_id))
-
 bunch = Classifier.bunch_with_targets(speeches=speeches, target_function=party_fn)
 data = naive_bayes.vectorizer.fit_transform(bunch.data) #.tocsr()#.toarray()
 target = array(bunch.target)
