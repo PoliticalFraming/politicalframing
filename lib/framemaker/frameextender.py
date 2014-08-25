17ef883bda53305ed5bb39023f2246176217c99b
Mehta, Dhrumil <dhrumil.mehta@gmail.com>
2014-08-24 22:35:10 -0400

    add definitions and examples to extended frame

--- lib/framemaker/frameextender.py
+++ lib/framemaker/frameextender.py
@@ -5,14 +5,29 @@ from preprocessor import removestops, removepunctuation
 from wordnettools import gethyponymsrecursive
 
 ## HOLY CRAP WEIGHTED FRAMES !!!
+def flatten(l):
+  return  [item for sublist in l for item in sublist]
 
 def frameextension(synset):
-    extension = []
-    for hyponym in gethyponymsrecursive(synset):
-        for word in removestops(removepunctuation(hyponym.definition)).split():
-            extension.append(word)
-    return set(extension)
+	extension = []
+
+	# Get words in definition of this synset
+	extension += removestops(removepunctuation(synset.definition)).split()
+	# Get words in example of this synset
+	wordlists = [removestops(removepunctuation(example)).split() for example in synset.examples]
+	extension += reduce(lambda x,y: x+y, wordlists, [])
+
+	# Get hyponyms recursively
+	for hyponym in gethyponymsrecursive(synset):
+		definitionwords = removestops(removepunctuation(hyponym.definition)).split()
+		examplewordlists = [removestops(removepunctuation(example)).split() for example in hyponym.examples]
+		examplewords = reduce(lambda x,y: x+y, wordlists, [])
+		extension += definitionwords
+		extension += examplewords
+
+	return set(extension)
 
 if __name__ == "__main__":
-    crime = wordnet.synset('crime.n.01')
-    print frameextension(crime)
+	crime = wordnet.synset('crime.n.01')
+	print frameextension(crime)
+	import pdb; pdb.set_trace()
